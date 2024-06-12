9dd6fedf741ee0b121e4c6c4aa06752fd740e24a
Kohichi-Isogai <143569429+Kohichi-Isogai@users.noreply.github.com>
2024-06-12 17:50:37 +0900

    frontend Home作成

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -4,15 +4,35 @@ import { useNavigate } from "react-router-dom";
 export const Home = () => {
   const navigate = useNavigate();
   return (
-    <Stack>
-      <div>新規作成</div>
-      <div
+    <Stack align="center">
+      <Stack
+        align="center"
+        justify="center"
+        gap="xs"
+        style={{ border: " 2px solid #000", width: "60%", height: "20vh" }}
+        onClick={() => {
+          navigate("/create");
+        }}
+      >
+        <div style={{ fontSize: "200%", fontWeight: "bold" }}>+</div>
+        <div>アイテム追加</div>
+      </Stack>
+      <Stack
+        align="center"
+        justify="center"
+        gap="xl"
+        style={{ border: " 2px solid #000", width: "60%", height: "20vh" }}
         onClick={() => {
           navigate("/items");
         }}
       >
-        一覧
-      </div>
+        <div style={{ fontWeight: "bold", paddingRight: "7px" }}>
+          <p style={{ margin: "0", height: "6px" }}>・ー</p>
+          <p style={{ margin: "0", height: "6px" }}>・ー</p>
+          <p style={{ margin: "0", height: "6px" }}>・ー</p>
+        </div>
+        <div>一覧</div>
+      </Stack>
       <div>ピックアップ</div>
     </Stack>
   );
--- frontend/src/page/HomePage.jsx
+++ frontend/src/page/HomePage.jsx
@@ -3,6 +3,7 @@ import { Home } from "../components/Home";
 export const HomePage = () => {
   return (
     <>
+      <h1>ホーム</h1>
       <Home />
     </>
   );
