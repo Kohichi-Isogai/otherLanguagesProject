1981de3eec527dc01ba63a1d3f1fc4d3a890f4ce
Kohichi-Isogai <143569429+Kohichi-Isogai@users.noreply.github.com>
2024-06-13 21:51:33 +0900

    backend post put deleteできない問題解決

--- backend/src/main/kotlin/com/example/consumptionDateChecker/ConsumptionController.kt
+++ backend/src/main/kotlin/com/example/consumptionDateChecker/ConsumptionController.kt
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.client.RestTemplate
 
-@CrossOrigin(origins = ["http://localhost:8080/"], maxAge = 3600)
+//@CrossOrigin(origins = ["http://localhost:8080/"], maxAge = 3600)
 @RestController
 class ConsumptionController(
     @Autowired val consumptionRepository: consumptionRepository,
