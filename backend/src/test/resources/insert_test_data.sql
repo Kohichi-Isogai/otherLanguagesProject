2adb9e511b85280bf9f7247a9d289069e6b85031
Kohichi-Isogai <143569429+Kohichi-Isogai@users.noreply.github.com>
2024-06-13 21:54:29 +0900

    backend test data 日付修正 コミットし忘れ

--- backend/src/test/resources/insert_test_data.sql
+++ backend/src/test/resources/insert_test_data.sql
@@ -1,4 +1,4 @@
 TRUNCATE TABLE users CASCADE;
 TRUNCATE TABLE items;
 INSERT INTO users VALUES(1, 'testUser','password');
-INSERT INTO items VALUES(1, 'アサヒ飲料 おいしい水 天然水','https://item-shopping.c.yimg.jp/i/g/misono-support_b5-696',10,'2024-06-09 00:00:00',1);
+INSERT INTO items VALUES(1, 'アサヒ飲料 おいしい水 天然水','https://item-shopping.c.yimg.jp/i/g/misono-support_b5-696',10,'2024-07-09 00:00:00',1);
