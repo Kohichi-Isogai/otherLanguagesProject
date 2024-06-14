bf472bd2fb238808c5502a1130aab3959feaaed4
Kohichi-Isogai <143569429+Kohichi-Isogai@users.noreply.github.com>
2024-06-14 11:31:47 +0900

    frontend アイテム一覧を日付順にソート

--- frontend/src/components/ItemList.jsx
+++ frontend/src/components/ItemList.jsx
@@ -9,36 +9,44 @@ export const ItemList = (props) => {
   return (
     <>
       <Stack justify="center">
-        {data.map((data, index) => {
-          return (
-            <div
-              key={index}
-              style={{ border: " 2px solid #000" }}
-              onClick={() => {
-                navigate(`/items/${data.id}`);
-              }}
-            >
-              <Group>
-                <img src={data.image_url} style={{ width: "30%" }} />
-                <Stack align="flex-start" style={{ width: "60%" }}>
-                  <div
-                    style={{
-                      overflowX: "scroll",
-                      whiteSpace: "nowrap",
-                      width: "90%",
-                      paddingRight: "10px",
-                      textAlign: "left",
-                    }}
-                  >
-                    品名：{data.item}
-                  </div>
-                  <div>期限：{data.limit_date}</div>
-                  <div>数量：{data.quantity}</div>
-                </Stack>
-              </Group>
-            </div>
-          );
-        })}
+        {data
+          .sort((a, b) => {
+            if (a.limit_date > b.limit_date) {
+              return 1;
+            } else {
+              return -1;
+            }
+          })
+          .map((data, index) => {
+            return (
+              <div
+                key={index}
+                style={{ border: " 2px solid #000" }}
+                onClick={() => {
+                  navigate(`/items/${data.id}`);
+                }}
+              >
+                <Group>
+                  <img src={data.image_url} style={{ width: "30%" }} />
+                  <Stack align="flex-start" style={{ width: "60%" }}>
+                    <div
+                      style={{
+                        overflowX: "scroll",
+                        whiteSpace: "nowrap",
+                        width: "90%",
+                        paddingRight: "10px",
+                        textAlign: "left",
+                      }}
+                    >
+                      品名：{data.item}
+                    </div>
+                    <div>期限：{data.limit_date}</div>
+                    <div>数量：{data.quantity}</div>
+                  </Stack>
+                </Group>
+              </div>
+            );
+          })}
       </Stack>
 
       <div
